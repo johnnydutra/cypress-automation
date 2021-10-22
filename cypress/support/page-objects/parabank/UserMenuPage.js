ee70b6c056686991e1d0256fa00c717e569e1a64
Johnny Dutra <johnny.dutra@gmail.com>
2021-10-22 20:35:22 -0300

    Start locators for UserMenuPage

---
+++ cypress/support/page-objects/parabank/UserMenuPage.js
@@ -0,0 +1,16 @@
+class UserMenuPage {
+
+    welcomeMessageText = cy.get();
+    menuHeaderText = cy.get();
+    
+    openNewAccountLink = cy.get();
+    accountOverviewLink = cy.get();
+    transferFundsLink = cy.get();
+    billPayLink = cy.get();
+    findTransactionsLink = cy.get();
+    updateContactInfoLink = cy.get();
+    requestLoanLink = cy.get();
+    logOutLink = cy.get();
+
+}
+export default UserMenuPage
