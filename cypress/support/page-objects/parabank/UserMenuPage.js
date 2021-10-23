d9f6a8dc7a48b7fefa1909a535bed1f050d4716c
Johnny Dutra <johnny.dutra@gmail.com>
2021-10-23 15:47:01 -0300

    Add locators and methods to UserMenuPage

--- cypress/support/page-objects/parabank/UserMenuPage.js
+++ cypress/support/page-objects/parabank/UserMenuPage.js
@@ -1,16 +1,48 @@
 class UserMenuPage {
 
-    welcomeMessageText = cy.get();
-    menuHeaderText = cy.get();
+    welcomeMessageText = cy.get(".smallText");
+    menuHeaderText = cy.get("h2");
     
-    openNewAccountLink = cy.get();
-    accountOverviewLink = cy.get();
-    transferFundsLink = cy.get();
-    billPayLink = cy.get();
-    findTransactionsLink = cy.get();
-    updateContactInfoLink = cy.get();
-    requestLoanLink = cy.get();
-    logOutLink = cy.get();
+    openNewAccountLink = cy.xpath("//div[@id='leftPanel']//a[text()='Open New Account']");
+    accountOverviewLink = cy.xpath("//div[@id='leftPanel']//a[text()='Accounts Overview']");
+    transferFundsLink = cy.xpath("//div[@id='leftPanel']//a[text()='Transfer Funds']");
+    billPayLink = cy.xpath("//div[@id='leftPanel']//a[text()='Bill Pay']");
+    findTransactionsLink = cy.xpath("//div[@id='leftPanel']//a[text()='Find Transactions']");
+    updateContactInfoLink = cy.xpath("//div[@id='leftPanel']//a[text()='Update Contact Info']");
+    requestLoanLink = cy.xpath("//div[@id='leftPanel']//a[text()='Request Loan']");
+    logOutLink = cy.xpath("//div[@id='leftPanel']//a[text()='Log Out']");
+
+    accessOpenNewAccount() {
+        this.openNewAccountLink.click();
+    };
+
+    accessAccountsOverview() {
+        this.accountOverviewLink.click();
+    };
+
+    accessTransferFunds() {
+        this.transferFundsLink.click();
+    };
+
+    accessBillPay() {
+        this.billPayLink.click();
+    };
+
+    accessFindTransactions() {
+        this.findTransactionsLink.click();
+    };
+
+    accessUpdateContactInfo() {
+        this.updateContactInfoLink.click();
+    };
+
+    accessRequestLoan() {
+        this.requestLoanLink.click();
+    };
+
+    logUserOut() {
+        this.logOutLink.click()
+    };
 
 }
 export default UserMenuPage
