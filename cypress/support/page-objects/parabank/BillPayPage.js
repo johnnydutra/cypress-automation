643df11041b351a2114147868e1dc5cabade0a10
Johnny Dutra <johnny.dutra@gmail.com>
2021-10-23 13:58:46 -0300

    Add basic locators and methods to BillPayPage

--- cypress/support/page-objects/parabank/BillPayPage.js
+++ cypress/support/page-objects/parabank/BillPayPage.js
@@ -1,47 +1,105 @@
 class BillPayPage {
 
-    billPayAppDiv = cy.get();
-    billPayAppHeader = cy.get();
+    billPayAppDiv = cy.get(".ng-scope");
+    billPayAppHeader = cy.get("h1");
 
-    payeeNameLabel = cy.get();
+    payeeNameLabel = cy.xpath("(//table[@class='form2']//b)[1]");
     payeeNameInput = cy.get("#payee.name");
-    payeeNameError = cy.get();
+    payeeNameError = cy.xpath("(//span[@class='error'])[1]");
 
-    addressLabel = cy.get();
+    addressLabel = cy.xpath("(//table[@class='form2']//b)[2]");
     addressInput = cy.get("#payee.address.street");
-    addressError = cy.get();
+    addressError = cy.xpath("(//span[@class='error'])[2]");
 
-    cityLabel = cy.get();
+    cityLabel = cy.xpath("(//table[@class='form2']//b)[3]");
     cityInput = cy.get("#payee.address.city");
-    cityError = cy.get();
+    cityError = cy.xpath("(//span[@class='error'])[3]");
 
-    stateLabel = cy.get();
+    stateLabel = cy.xpath("(//table[@class='form2']//b)[4]");
     stateInput = cy.get("#payee.address.state");
-    stateError = cy.get();
+    stateError = cy.xpath("(//span[@class='error'])[4]");
 
-    zipCodeLabel = cy.get();
+    zipCodeLabel = cy.xpath("(//table[@class='form2']//b)[5]");
     zipCodeInput = cy.get("#payee.address.zipCode");
-    zipCodeError = cy.get();
+    zipCodeError = cy.xpath("(//span[@class='error'])[5]");
 
-    phoneNumberLabel = cy.get();
+    phoneNumberLabel = cy.xpath("(//table[@class='form2']//b)[6]");
     phoneNumberInput = cy.get("#payee.phoneNumber");
-    phoneNumberError = cy.get();
+    phoneNumberError = cy.xpath("(//span[@class='error'])[6]");
 
-    accountNumberLabel = cy.get();
+    accountNumberLabel = cy.xpath("(//table[@class='form2']//b)[7]");
     accountNumberInput = cy.get("#payee.accountNumber");
-    accountNumberError = cy.get();
+    accountNumberError = cy.xpath("(//span[@class='error'])[7]");
 
-    verifyAccountLabel = cy.get();
+    verifyAccountLabel = cy.xpath("(//table[@class='form2']//b)[8]");
     verifyAccountInput = cy.get("[@name=verifyAccount]");
-    verifyAccountError = cy.get();
+    verifyAccountError = cy.xpath("(//span[@class='error'])[8]");
 
-    amountLabel = cy.get();
+    amountLabel = cy.xpath("(//table[@class='form2']//b)[9]");
     amountInput = cy.get("[@name=amount]");
-    amountError = cy.get();  
+    amountError = cy.xpath("(//span[@class='error'])[9]");  
 
-    fromAccountLabel = cy.get();
-    fromAccountInput = cy.get("[@name=fromAccountId");
-    fromAccountError = cy.get();
+    fromAccountLabel = cy.xpath("(//table[@class='form2']//b)[10]");
+    fromAccountSelect = cy.get("[@name=fromAccountId");
+
+    sendPaymentButton = cy.xpath("//input[@value='Send Payment']");
+
+    inputPayeeName(payeeName) {
+        this.payeeNameInput.clear().type(payeeName);
+    };
+
+    inputAddress(address) {
+        this.addressInput.clear().type(address);
+    };
+
+    inputCity(city) { 
+        this.cityInput.clear().type(city);
+    };
+
+    inputState(state) {
+        this.stateInput.clear().type(state);
+    };
+
+    inputZipCode(zipCode) {
+        this.zipCodeInput.clear().type(zipCode);
+    };
+
+    inputPhoneNumber(phoneNumber) {
+        this.phoneNumberInput.clear().type(phoneNumber);
+    };
+
+    inputAccountNumber(account) {
+        this.accountNumberInput.clear().type(account);
+    };
+
+    inputVerifyAccount(account) {
+        this.verifyAccountInput.clear().type(account);
+    };
+
+    inputAmount(amount) {
+        this.amountInput.clear().type(amount);
+    };
+
+    clickSendPaymentButton() {
+        this.sendPaymentButton.click();
+    };
+
+    fillForm(payeeName, address, city, state, zipCode, phoneNumber, accountNumber, verifyAccount, amount) {
+        this.inputPayeeName(payeeName);
+        this.inputAddress(address);
+        this.inputCity(city);
+        this.inputState(state);
+        this.inputZipCode(zipCode);
+        this.inputPhoneNumber(phoneNumber);
+        this.inputAccountNumber(accountNumber);
+        this.inputVerifyAccount(verifyAccount);
+        this.inputAmount(amount);
+    };
+
+    fillAndSubmitForm(payeeName, address, city, state, zipCode, phoneNumber, accountNumber, verifyAccount, amount) {
+        this.fillForm(payeeName, address, city, state, zipCode, phoneNumber, accountNumber, verifyAccount, amount);
+        this.clickSendPaymentButton();
+    };
 
 }
 export default BillPayPage
