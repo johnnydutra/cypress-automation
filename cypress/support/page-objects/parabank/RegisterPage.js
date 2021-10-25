0f40899d255fffcf43ff27c22a9fd68750fafefe
Johnny Dutra <johnny.dutra@gmail.com>
2021-10-25 18:43:53 -0300

    Refactor RegisterPage

--- cypress/support/page-objects/parabank/RegisterPage.js
+++ cypress/support/page-objects/parabank/RegisterPage.js
@@ -1,111 +1,124 @@
-class RegisterPage {
+export class RegisterPage {
 
-    firstNameLabel = cy.get();
-    firstNameInput = cy.get("#firstName");
-    firstNameError = cy.get("#firstName.errors");
+    // Locators
 
-    lastNameLabel = cy.get();
-    lastNameInput = cy.get("#lastName");
-    lastNameError = cy.get("#address.street.errors");
+    getRegisterAppDiv = () => cy.get("#customerForm");
+    getRegisterAppHeader = () => cy.get("h1");
 
-    addressLabel = cy.get();
-    addressInput = cy.get("#address.street");
-    addressError = cy.get("#address.street.errors");
+    getFirstNameLabel = () => cy.xpath("(//form[@id='customerForm']//b)[1]");
+    getFirstNameInput = () => cy.xpath("//input[@id='customer.firstName']");
+    getFirstNameError = () => cy.xpath("//span[@id='customer.firstName.errors']");
 
-    cityLabel = cy.get();
-    cityInput = cy.get("#address.city");
-    cityError = cy.get("#address.city.errors");
+    getLastNameLabel = () => cy.xpath("(//form[@id='customerForm']//b)[2]");
+    getLastNameInput = () => cy.xpath("//input[@id='customer.lastName']");
+    getLastNameError = () => cy.xpath("//span[@id='customer.lastName.errors']");
 
-    stateLabel = cy.get();
-    stateInput = cy.get("#address.state");
-    stateError = cy.get("#address.state.errors");
+    getAddressLabel = () => cy.xpath("(//form[@id='customerForm']//b)[3]");
+    getAddressInput = () => cy.xpath("//input[@id='customer.address.street']");
+    getAddressError = () => cy.xpath("//span[@id='customer.address.street.errors']");
 
-    zipCodeLabel = cy.get();
-    zipCodeInput = cy.get("#address.zipCode");
-    zipCodeError = cy.get("#address.zipCode.errors");
+    getCityLabel = () => cy.xpath("(//form[@id='customerForm']//b)[4]");
+    getCityInput = () => cy.xpath("//input[@id='customer.address.city']");
+    getCityError = () => cy.xpath("//span[@id='customer.address.city.errors']");
 
-    phoneNumberLabel = cy.get();
-    phoneNumberInput = cy.get("#customer.phoneNumber");
+    getStateLabel = () => cy.xpath("(//form[@id='customerForm']//b)[5]");
+    getStateInput = () => cy.xpath("//input[@id='customer.address.state']");
+    getStateError = () => cy.xpath("//span[@id='customer.address.state.errors']");
 
-    socialSecurityNumberLabel = cy.get();
-    socialSecurityNumberInput = cy.get("#ssn");
-    socialSecurityNumberError = cy.get("#ssn.errors");
+    getZipCodeLabel = () => cy.xpath("(//form[@id='customerForm']//b)[6]");
+    getZipCodeInput = () => cy.xpath("//input[@id='customer.address.zipCode']");
+    getZipCodeError = () => cy.xpath("//span[@id='customer.address.zipCode.errors']");
 
-    usernameLabel = cy.get();
-    usernameInput = cy.get("#customer.username");
-    usernameError = cy.get("#customer.username.errors");
+    getPhoneNumberLabel = () => cy.xpath("(//form[@id='customerForm']//b)[7]");
+    getPhoneNumberInput = () => cy.xpath("//input[@id='customer.phoneNumber']");
 
-    passwordLabel = cy.get();
-    passwordInput = cy.get("#customer.password");
-    passwordError = cy.get("#customer.password.errors");
+    getSocialSecurityNumberLabel = () => cy.xpath("(//form[@id='customerForm']//b)[8]");
+    getSocialSecurityNumberInput = () => cy.xpath("//input[@id='customer.ssn']");
+    getSocialSecurityNumberError = () => cy.xpath("//span[@id='customer.ssn.errors']");
 
-    confirmPasswordLabel = cy.get();
-    confirmPasswordInput = cy.get("#repeatedPassword");
-    confirmPasswordError = cy.get("#repeatedPassword.errors");
+    getUsernameLabel = () => cy.xpath("(//form[@id='customerForm']//b)[9]");
+    getUsernameInput = () => cy.xpath("//input[@id='customer.username']");
+    getUsernameError = () => cy.xpath("//span[@id='customer.username.errors']");
 
-    registerButton = cy.get();
+    getPasswordLabel = () => cy.xpath("(//form[@id='customerForm']//b)[10]");
+    getPasswordInput = () => cy.xpath("//input[@id='customer.password']");
+    getPasswordError = () => cy.xpath("//span[@id='customer.password.errors']");
+
+    getConfirmPasswordLabel = () => cy.xpath("(//form[@id='customerForm']//b)[11]");
+    getConfirmPasswordInput = () => cy.get("#repeatedPassword");
+    getConfirmPasswordError = () => cy.xpath("//span[@id='repeatedPassword.errors']");
+
+    getRegisterButton = () => cy.xpath("//input[@value='Register']");
+
+    getConfirmationMessage = () => cy.xpath("(//div[@id='rightPanel']//p)[1]");
+
+    // Actoions
 
     inputFirstName(firstName) {
-        this.firstNameInput.clear().type(firstName);
+        this.getFirstNameInput().clear().type(firstName);
     };
 
     inputLastName(lastName) {
-        this.lastNameInput.clear().type(lastName);
+        this.getLastNameInput().clear().type(lastName);
     };
 
     inputAddress(address) {
-        this.addressInput.clear().type(address);
+        this.getAddressInput().clear().type(address);
     };
 
     inputCity(city) {
-        this.cityInput.clear().type(city);
+        this.getCityInput().clear().type(city);
     };
 
     inputState(state) {
-        this.stateInput.clear().type(state);
+        this.getStateInput().clear().type(state);
     };
 
     inputZipCode(zipCode) {
-        this.zipCodeInput.clear().type(zipCode);
+        this.getZipCodeInput().clear().type(zipCode);
     };
 
-    inputSocialSecurityNUmber(socialSecurityNumber) {
-        this.socialSecurityNumberInput.clear().type(socialSecurityNumber);
+    inputPhoneNumber(phoneNumber) {
+        this.getPhoneNumberInput().clear().type(phoneNumber);
+    };
+
+    inputSocialSecurityNumber(socialSecurityNumber) {
+        this.getSocialSecurityNumberInput().clear().type(socialSecurityNumber);
     };
 
     inputUsername(username) {
-        this.usernameInput.clear().type(username);
+        this.getUsernameInput().clear().type(username);
     };
 
     inputPassword(password) {
-        this.passwordInput.clear().input(password);
+        this.getPasswordInput().clear().type(password);
     };
 
     inputConfirmPassword(password) {
-        this.confirmPasswordInput.clear().input(password);
+        this.getConfirmPasswordInput().clear().type(password);
     };
 
     clickRegisterButton() {
-        this.registerButton.click();
+        this.getRegisterButton().click();
     };
 
-    fillForm(firstName, lastName, address, city, state, zipCode, socialSecurityNumber, username, password, passwordConfirmation) {
+    fillForm(firstName, lastName, address, city, state, zipCode, phoneNumber, socialSecurityNumber, username, password, passwordConfirmation) {
         this.inputFirstName(firstName);
         this.inputLastName(lastName);
         this.inputAddress(address);
         this.inputCity(city);
         this.inputState(state);
         this.inputZipCode(zipCode);
+        this.inputPhoneNumber(phoneNumber);
         this.inputSocialSecurityNumber(socialSecurityNumber);
         this.inputUsername(username);
         this.inputPassword(password);
         this.inputConfirmPassword(passwordConfirmation);
     };
 
-    fillAndSubmitForm(firstName, lastName, address, city, state, zipCode, socialSecurityNumber, username, password, passwordConfirmation) {
-        this.fillForm(firstName, lastName, address, city, state, zipCode, socialSecurityNumber, username, password, passwordConfirmation);
+    fillAndSubmitForm(firstName, lastName, address, city, state, zipCode, phoneNumber, socialSecurityNumber, username, password, passwordConfirmation) {
+        this.fillForm(firstName, lastName, address, city, state, zipCode, phoneNumber, socialSecurityNumber, username, password, passwordConfirmation);
         this.clickRegisterButton();
     };
     
 };
-export default RegisterPage
